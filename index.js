0d5a7145fc439337bc83a7e7f59c3ef73ffd8c00
Marco Antônio Queiroz <marco.aq7@gmail.com>
2021-04-07 11:14:05 -0300

    SendMensage update

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Converter Convert Table to Multidimensional Array
 // https://www.seabreezecomputers.com/excel2array/
 
-var listaContatos = [
-    ["+55 (62) 91234-1234", "Fulano da Silva"], 
-    ["+55 (62) 91234-1234", "Zé da Silva"], 
+var contatos = [
+    ["+55 (62) 91234-1234", "1 Fulano da Silva"], 
+    ["+55 (62) 9111112385", "2 Fulano da Silva"], 
 ];
 
 
@@ -16,64 +16,73 @@ Está é uma mensagem de teste!
 `;
 
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+var managerAPP = {
+    header: document.querySelector('header'),
+    main: document.querySelector('#main'),
+    sleep: function(ms){
+        return new Promise(resolve => setTimeout(resolve, ms));
+    },
+    clear: function(){
+        var els = this.header.querySelectorAll('a.itemURL');
+        els.forEach(el=>el.remove())
+    },
+    validURL: function(){
+        var pop = document.querySelector("div._3SRfO")
+        if(pop.innerText.includes('inválido'))
+            throw "O número de telefone compartilhado através de url é inválido."
+        return false
+    },
+    clickSend: async function(){
+        await this.sleep(4000)
+        console.info("Click send...");
+        let buttons = this.main.querySelectorAll('button');
+        buttons[buttons.length -1].click();
+        this.clear()
+        return true
+    },
+    sendMessage:async function(url, autoSend=false) {
+        console.info("Open...");
+        const header = document.querySelector('header');
+        let a = document.createElement('a');
+        a.setAttribute('class','itemURL');
+        a.setAttribute('href', url);
+        header.appendChild(a);
+        a.click()
+        await this.sleep(500)
+        this.validURL()
+        return autoSend && await this.clickSend();
+    }, 
 }
 
-function sendMessage ( ) {
-    var main = document.querySelector('#main');
-    var event = new InputEvent('input', {
-        bubbles: true
-    });
-    var textbox = main.querySelector('div._2_1wd');
-    textbox.dispatchEvent(event);
-    var buttons = main.querySelectorAll('button');
-    buttons[buttons.length -1].click();
-}
-function deleteUL(header){
-    try {
-        var elUL = header.querySelector('ul.ulURL');
-        elUL.remove();
-    } catch (e) { }
-}
-function createUL( ){
-    var header = document.querySelector('header');
-    var ul = document.createElement('ul');
-    deleteUL(header);
-    ul.setAttribute('class', 'ulURL');
-    header.appendChild(ul);
-    return ul;
-}
-function createLI(listContact){
-    var ul = createUL( );
-    listContact.forEach(([number, name])=>{
-        var li = document.createElement('li');
-        var a = document.createElement('a');
-        
-        let msg = message.replace(/{{nome}}/g, name);
+async function init(){
+
+    console.info('Criando links...');
+
+    let logs = {errors:[]}
+    
+    for (const [number, name] of contatos) {
+
         let nb = number.replace(/([()+. -])/g, '');
-        a.setAttribute('class','itemURL')
-        a.setAttribute('href','https://wa.me/'+nb+'&text='+encodeURIComponent(msg));
-        li.append(a);
-        ul.appendChild(li);
-    });
-    return document.querySelectorAll('a.itemURL');
+        let msg = message.replace(/{{nome}}/g, name);
+        let url = 'https://wa.me/'+nb+'&text='+encodeURIComponent(msg)
+
+        try {
+            
+            console.info('Enviando mensagem para: ', url.split("&")[0]);
+            await managerAPP.sendMessage(url, true)
+
+        } catch (e) {
+            logs.errors.push([number, name, e]) 
+            console.info(`Erro ao enviar mensagem para ${nb}, ${url.split("&")[0]}`)
+            console.warn("Erro encontrado:", e)
+        }
+    }
+
+    console.group("Saída:");
+    console.error("Erros encontrados:")
+    console.table(logs.errors, [2,3,4])
+    console.groupEnd();
+    
 }
 
-(async ()=>{
-
-    console.log('Criando links...');
-    
-    var itensURL = createLI(listaContatos);
-    
-    for (const item of itensURL) {
-        
-        console.log('Enviando mensagem para: ', item.href);
-        
-        item.click();
-        await sleep(3000);
-        
-        sendMessage()
-        await sleep(2000);
-    }
-})()
+init()
